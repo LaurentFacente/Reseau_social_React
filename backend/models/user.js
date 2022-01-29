b134b0aa3d95f8bf5f787e23c87a095a548235fe
Laurent Facente <facentelaurent@gmail.com>
2022-01-29 22:53:16 +0100

    Correction erreur

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,28 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('User', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: {
-          msg:"Ce nom d'utilisateur est deja pris !"
-        }
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      isAdmin: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false
-      },
-    }, {
-      timestamps: true,
-      createdAt: 'created',
-      updatedAt: false
-    })
-  }
+  return sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg:"Ce nom d'utilisateur est deja pris !"
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+  }, {
+    timestamps: true,
+    createdAt: 'created',
+    updatedAt: false
+  })
+}
