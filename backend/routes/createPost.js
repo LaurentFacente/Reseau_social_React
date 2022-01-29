fec1fb319b9278745fd2dd6f91b5f60845fbbeec
Laurent Facente <facentelaurent@gmail.com>
2022-01-29 19:59:12 +0100

    Ajout de l'authentification par token sur les routes qui fonctionne bien

--- backend/routes/createPost.js
+++ backend/routes/createPost.js
@@ -1,7 +1,8 @@
 const { Post } = require('../db/sequelize')
+const auth = require('../auth/auth')
   
 module.exports = (app) => {
-  app.post('/api/posts', (req, res) => {
+  app.post('/api/posts', auth, (req, res) => {
     Post.create(req.body)
       .then(post => {
         const message = `Le post ${req.body.title} a bien été crée.`
--- backend/routes/findAllPost.js
+++ backend/routes/findAllPost.js
@@ -1,7 +1,8 @@
 const { Post } = require('../db/sequelize')
+const auth = require('../auth/auth')
   
 module.exports = (app) => {
-  app.get('/api/posts', (req, res) => {
+  app.get('/api/posts', auth, (req, res) => {
     Post.findAll()
       .then(posts => {
         const message = 'La liste des post a bien été récupérée.'
