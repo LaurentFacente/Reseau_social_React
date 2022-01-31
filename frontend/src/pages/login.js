ab7d289b151b819e5087db186cdf3168c4668f17
Laurent Facente <facentelaurent@gmail.com>
2022-01-31 14:32:27 +0100

    Ajout du style css pour la page de login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -3,8 +3,19 @@ import "./Login.css";
 
 const Login = () => {
     return (
-        <div>
-           <p> Page de Login de l'app</p> 
+        <div className="loginPage">
+           <img className="logo" src="../images/icon-left-font-monochrome-white.png" alt="groupomania icon" width="300px"></img>
+           <h1>Bienvenue sur le reseau social de Groupomania</h1>
+           <div className="login-page">
+                <div className="form">
+                    <form class="login-form">
+                         <input type="text" placeholder="username"/>
+                         <input type="password" placeholder="password"/>
+                        <button>Login</button>
+                         <p className="message">Si c'est votre premiere connexion, cette action vous créra un compte.</p>
+                    </form>
+                 </div>
+            </div>
         </div>
     );
 };
