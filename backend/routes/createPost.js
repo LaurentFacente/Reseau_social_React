503e5efdd382c3b66182a006b704b29d344589af
Laurent Facente <facentelaurent@gmail.com>
2022-02-04 03:01:48 +0100

    Quelques modifs pour l'affichage des post en front

--- backend/routes/createPost.js
+++ backend/routes/createPost.js
@@ -1,8 +1,9 @@
 const { Post } = require('../db/sequelize')
 const auth = require('../auth/auth')
+const multer = require('../utils/multer-config')
   
 module.exports = (app) => {
-  app.post('/api/posts', auth, (req, res) => {
+  app.post('/api/posts', auth, multer, (req, res) => {
     Post.create(req.body)
       .then(post => {
         const message = `Le post ${req.body.title} a bien été crée.`
--- backend/routes/findAllPost.js
+++ backend/routes/findAllPost.js
@@ -2,11 +2,11 @@ const { Post } = require('../db/sequelize')
 const auth = require('../auth/auth')
   
 module.exports = (app) => {
-  app.get('/api/posts', auth, (req, res) => {
+  app.get('/api/posts', (req, res) => {
     Post.findAll()
       .then(posts => {
-        const message = 'La liste des post a bien été récupérée.'
-        res.json({ message, data: posts })
+    
+        res.json({ data: posts })
       })
   })
 }
