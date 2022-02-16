a90c29097f3f00cff2f0833f4b2f3fdc5db333e8
Laurent Facente <facentelaurent@gmail.com>
2022-02-16 15:54:46 +0100

    Delete user / ok

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,6 +6,23 @@ import axios from 'axios';
 
 
 function Header() {
+
+  
+  
+  const deleteProfil = async () => {
+    console.log(localStorage['Id'])
+    const id = localStorage['Id']
+    
+    try {
+      const res = await axios.delete("http://localhost:3000/api/user/" + id )
+      console.log(`L'user ${ id } à été suprimé`)
+      localStorage.clear();
+      window.location = "/Register";
+      
+    } catch (error) {
+      alert(error.message)
+    }
+  }
  
 
   return (
@@ -14,6 +31,9 @@ function Header() {
     <Link to="/Feed">
       <img className="logo" src="./images/icon-left-font-monochrome-white.png" alt="Groupomania" />
       </Link>
+    </div>
+    <div>
+      <a className='delete_profil' onClick={deleteProfil} href="http://localhost:3001/Register">Suprimer mon profil</a>
     </div>
     <div>
       <a className='login_button' onClick={() =>localStorage.clear()} href="http://localhost:3001/">Logout</a>
