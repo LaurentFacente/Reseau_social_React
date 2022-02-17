8ca87a432c02335b92fad37063f88ba33b01841b
Laurent Facente <facentelaurent@gmail.com>
2022-02-17 14:20:15 +0100

    amelioration UI

--- frontend/src/pages/Article.js
+++ frontend/src/pages/Article.js
@@ -11,15 +11,30 @@ import { FaTrashAlt } from 'react-icons/fa';
 
 
 const Article = () => {
+ 
+ const Admin = () =>{
+ const adminId = '1';
+ const id = localStorage['Id']; 
+ console.log(adminId)
+ console.log(id)
+ if(adminId != id){
+   return null
+ }
+return(
+ <div className='btn_suppr' onClick={deleteArticle}><FaTrashAlt /></div>
+)
+}
+
+
+  // On recuperer l'ID du post grace a useParams
     const { id } = useParams();
 
     useEffect(() => {
-        console.log('useEffect a été appelé')
         getArticle()
       },[])
-  
+    // On defini un etat pour l'article
       const [article, setArticle] = useState([])
-    
+    //Logique de recuperation de l'article
       const getArticle = async () => {
         try {
           const res = await axios.get("http://localhost:3000/api/posts/" + id )
@@ -30,7 +45,7 @@ const Article = () => {
           alert(error.message)
         }
       }
-
+      // Gestion de la logique de supression d'un article
       const deleteArticle = async () => {
         try {
           const res = await axios.delete("http://localhost:3000/api/posts/" + id )
@@ -42,25 +57,51 @@ const Article = () => {
           alert(error.message)
         }
       }
-
+      // Gestion de la logique de supression d'un User
+      const deleteProfil = async () => {
+        // On recupere l'Id de l'utilisateur dans le local storage
+        console.log(localStorage['Id'])
+        const id = localStorage['Id']
+        
+        try {
+          // On declanche la supression, on clear le local storage et on redirige l'utilisateur
+          const res = await axios.delete("http://localhost:3000/api/user/" + id )
+          console.log(`L'user ${ id } à été suprimé`)
+          localStorage.clear();
+          window.location = "/Register";
+          
+        } catch (error) {
+          alert(error.message)
+        }
+      }
 
   return (
     <div>
-        <Header></Header>
+        <header>
+          <div>
+          <Link to="/Feed">
+          <img className="logo" src="./images/icon-left-font-monochrome-white.png" alt="Groupomania" />
+          </Link>
+          </div>
+          <div>
+          <a className='delete_profil' onClick={deleteProfil} href="http://localhost:3001/Register">Suprimer mon profil</a> 
+          </div>
+          <div>
+          <a className='login_button' onClick={() =>localStorage.clear()} href="http://localhost:3001/">Déconnexion</a>
+          </div> 
+          </header>
        
-       <div key={article.id} className="container_article">
-       <div className='btn_suppr' onClick={deleteArticle}><FaTrashAlt /></div>
-                <div className="square_article">
-                     <img src={article.attachment} className="mask_article"></img>
-                     <div className="h1_article">{article.title}</div>
-                     <p>{article.content}</p>
-                </div>
+              <div key={article.id} className="container_article">
+               
+                  <Admin />
+                    <div className="square_article">
+                      <img src={article.attachment} className="mask_article"></img>
+                      <div className="h1_article">{article.title}</div>
+                      <p>{article.content}</p>
+                    </div>
               </div>
-              
               <Footer></Footer>
-              
     </div>
-  
   )
 }
 
--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -3,51 +3,43 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header.js";
 import { Link } from "react-router-dom";
-
-
-
-
-
+import Footer from "../components/Footer";
 
 
 
 const Posts = () => {
-
+    // On innitialise la recuperation des posts en BDD
     useEffect(() => {
-      console.log('useEffect a été appelé')
       getPosts()
-    },[])
+    })
 
+    //On defini l'etat des posts
     const [posts, setPosts] = useState([])
   
-  
-
+    //Appel a l'API pour recuperer les posts
     const getPosts = async () => {
       try {
         const res = await axios.get("http://localhost:3000/api/posts")
         setPosts(res.data.data)
-        
       } catch (error) {
         alert(error.message)
-        
       }
     }
-
+    // Gestion de la soumission d'un nouveau post
+    // On ecoute les champs de creation d'un post
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
     const [attachment, setAttachment] = useState("")
     const [prevPicture, setPrevPictture] = useState('')
     
-    
-    
-
+    // Gestion de la previsualisation et de la soumission d'une photo
     const handlePicture = (e) => {
       setPrevPictture(URL.createObjectURL(e.target.files[0])) 
       setAttachment(URL.createObjectURL(e.target.files[0]))
      
     
       }
-    
+    // On ecoute le bouton de soumission et on declanche la logique 
     const handlePost = async (e) => {
         const data = new FormData();
         data.append('title', title)
@@ -55,23 +47,51 @@ const Posts = () => {
         data.append('attachment', prevPicture)
         console.log(data)
         e.preventDefault();
+    // Appel a l'API de la route CreatePost 
         axios({method: "post", url: 'http://localhost:3000/api/posts',
           data: {title,content,attachment},
         })
     }
-
-    
+    // Gestion de la logique de supression d'un User
+    const deleteProfil = async () => {
+    // On recupere l'Id de l'utilisateur dans le local storage
+      console.log(localStorage['Id'])
+      const id = localStorage['Id']
+      
+      try {
+        // On declanche la supression, on clear le local storage et on redirige l'utilisateur 
+        const res = await axios.delete("http://localhost:3000/api/user/" + id )
+        console.log(`L'user ${ id } à été suprimé`)
+        localStorage.clear();
+        window.location = "/Register";
+        
+      } catch (error) {
+        alert(error.message)
+      }
+    }
     return (
           
       <div>
-        <Header />
-        <div className="container">  
-          <form 
-          id="contact"
-          action="/api/posts" 
-          method="POST"
-          encType="multipart/form-data">
-          <p className="creez">Creez et partager un article avec vos collaborateurs</p>
+        <header>
+              <div>
+                <Link to="/Feed">
+                <img className="logo" src="./images/icon-left-font-monochrome-white.png" alt="Groupomania" />
+                </Link>
+              </div>
+              <div>
+                <a className='delete_profil' onClick={deleteProfil} href="http://localhost:3001/Register">Suprimer mon profil</a> 
+                </div>
+                <div>
+                <a className='login_button' onClick={() =>localStorage.clear()} href="http://localhost:3001/">Déconnexion</a>
+            </div> 
+        </header>
+          <div className="container">  
+              <form 
+              id="contact"
+              action="/api/posts" 
+              method="POST"
+              encType="multipart/form-data">
+              <p className="creez">Creez et partager un article avec vos collaborateurs</p>
               <textarea  
               name="title" 
               id="message" 
@@ -94,34 +114,29 @@ const Posts = () => {
                id="attachment"  
                onChange={(e) => handlePicture(e)}/>
               <div>
-                <img src={prevPicture}>
-                  </img>
-                  </div>
+                <img src={prevPicture}></img>
+              </div>
               </div>
               <button 
               name="submit" 
               type="submit" 
               onClick={handlePost}>Envoyer
               </button> 
-          </form>
-        </div>
+              </form>
+          </div>
 
             {posts.slice(0).reverse().map((post) => (
-              
               <Link to={`/${post.id}`} key={post.id} className="container">
                 <div className="square">
                      <img src={post.attachment} className="mask"></img>
                      <div className="h1">{post.title}</div>
-                     <p>{post.content}</p>
-                   
+                     <p>{post.content}</p>  
                 </div>
               </Link>
-            ))}
-             
+            ))}   
+            <Footer />
       </div>
-      
-    )
-    
+    )  
 };
 
 export default Posts;
